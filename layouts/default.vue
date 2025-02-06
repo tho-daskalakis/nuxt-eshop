61fde2ab0880d3c0f33851ebcb1259467100972c
thomas-d <tho.daskalakis@gmail.com>
2025-02-06 21:56:10 +0200

    create default layout

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Toolbar>
+      <template #start>
+        <Menu :model='items' />
+      </template>
+      <template #center></template>
+      <template #end></template>
+    </Toolbar>
+
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+const items = reactive([
+  { label: 'New', icon: 'mdi:add' },
+  { label: 'Search', icon: 'pi pi-search' }
+]);
+</script>
