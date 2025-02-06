7c99eaac694a7ac5f9c67b0da80c961a6984d303
thomas-d <tho.daskalakis@gmail.com>
2025-02-06 19:24:00 +0200

    install primevue UI library module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
+import Aura from '@primevue/themes/aura';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: '2025-02-06',
+  devtools: { enabled: true },
+  modules: [
+    '@primevue/nuxt-module'
+  ],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura
+      }
+    }
+  }
+});
