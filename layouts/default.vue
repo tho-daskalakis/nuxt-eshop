df86df69534f33c8e491ab6eb53d80b7e92c4ab0
thomas-d <tho.daskalakis@gmail.com>
2025-02-07 13:29:57 +0200

    add shopping cart icon

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
     <Toolbar>
-      <template #start>
-        <Menu :model='items' />
-      </template>
+      <template #start></template>
       <template #center></template>
-      <template #end></template>
+      <template #end>
+        <IconField>
+          <InputIcon class="pi pi-search" />
+          <InputText v-model="searchText" placeholder="Search" />
+        </IconField>
+
+        <Button type="button" icon="pi pi-shopping-cart" @click="toggleMenu"
+                aria-haspopup="menu" aria-controls="overlay_menu" />
+        <Menu ref="menu" id="overlay_menu" :model="items" :popup="true" />
+      </template>
     </Toolbar>
 
     <slot />
@@ -13,10 +20,22 @@
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { ref, reactive } from 'vue';
+
+// Search
+
+const searchText = ref('');
+
+
+// Menu
+
+const menu = ref();
 
 const items = reactive([
-  { label: 'New', icon: 'pi pi-plus' },
   { label: 'Search', icon: 'pi pi-search' }
 ]);
+
+const toggleMenu = (event: Event) => {
+  menu.value.toggle(event);
+};
 </script>
